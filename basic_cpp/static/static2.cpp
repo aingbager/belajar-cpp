c712547a5c6546eece4a0fb061c62d0423ca99a4
aingbager <aingbager55@gmail.com>
2024-09-01 10:33:40 +0700

    static

---
+++ basic_cpp/static/static2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+class Angka{
+  public:
+  static int nomor;
+  void cek(){
+    nomor ++;
+  }
+};
+
+int Angka::nomor = 0;
+
+int main (int argc, char *argv[]) {
+
+  Angka angka1;
+  Angka angka2;
+
+  std::cout << Angka::nomor << std::endl;
+  return 0;
+}

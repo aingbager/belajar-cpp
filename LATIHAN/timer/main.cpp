066234f9d705c57f50ceca4c700e04b99bd7c61d
aingbager <aingbager55@gmail.com>
2024-09-26 19:24:09 +0700

    timer

--- LATIHAN/timer/main.cpp
+++ LATIHAN/timer/main.cpp
@@ -1,15 +1,39 @@
-#include <iostream>
 #include <chrono>
+#include <cstdlib>
+#include <iostream>
 #include <thread>
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 
   int inputan;
 
   std::cout << "=====PROGRAM SHUTDOWN ====" << std::endl;
 
   std::cout << "\n1.shutdown. \n2.batal.\nmasukan inputan? 1/2: ";
-  std::cin>> inputan;
-  
+  std::cin >> inputan;
+
+  if (inputan == 1) {
+    std::cout << "komputer dimatikan dalam 10 detik." << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    for (int i = 10; i >= 0; i--) {
+      std::cout << i << std::endl;
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+      system("clear");
+
+      try {
+        if (i != 0) {
+          throw "error kode";
+        }
+      } catch (const char *message) {
+        std::cout << "err: " << message << std::endl;
+      }
+    }
+    system("systemctl poweroff");
+  } else if (inputan == 2) {
+    std::cout << "program dibatalkan." << std::endl;
+  } else {
+    std::cout << "inputan salah program dibatalkan..." << std::endl;
+  }
+
   return 0;
 }
